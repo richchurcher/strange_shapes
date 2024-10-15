0753c852555d04b9084b4740e45d33aea0f8e982
Rich Churcher <rich.churcher@gmail.com>
2024-10-15 19:57:56 +1300

    Add simple camera controls

--- src/game/camera.rs
+++ src/game/camera.rs
@@ -1,13 +1,39 @@
-use bevy::prelude::*;
+use std::{f32::consts::FRAC_PI_2, ops::Range};
+
+use bevy::{input::mouse::MouseMotion, prelude::*};
 
 use crate::{game::movement::MovementController, screens::Screen, AppSet};
 
+#[derive(Debug, Resource)]
+struct CameraSettings {
+    pub orbit_distance: f32,
+    pub pitch_speed: f32,
+    // Clamp pitch to this range
+    pub pitch_range: Range<f32>,
+    pub roll_speed: f32,
+    pub yaw_speed: f32,
+}
+
+impl Default for CameraSettings {
+    fn default() -> Self {
+        // Limiting pitch stops some unexpected rotation past 90° up or down.
+        let pitch_limit = FRAC_PI_2 - 0.01;
+        Self {
+            // These values are completely arbitrary, chosen because they seem to produce
+            // "sensible" results for this example. Adjust as required.
+            orbit_distance: 20.0,
+            pitch_speed: 0.003,
+            pitch_range: -pitch_limit..pitch_limit,
+            roll_speed: 1.0,
+            yaw_speed: 0.004,
+        }
+    }
+}
+
 pub(super) fn plugin(app: &mut App) {
+    app.init_resource::<CameraSettings>();
     app.add_systems(Startup, init);
-    app.add_systems(
-        Update,
-        record_player_directional_input.in_set(AppSet::RecordInput),
-    );
+    app.add_systems(Update, orbit.in_set(AppSet::PlayingUpdate));
 }
 
 fn init(mut commands: Commands) {
@@ -55,4 +81,52 @@ fn record_player_directional_input(
     for mut controller in &mut controller_query {
         controller.intent = intent;
     }
+}
+
+fn orbit(
+    mut camera: Query<&mut Transform, With<Camera>>,
+    camera_settings: Res<CameraSettings>,
+    mouse_buttons: Res<ButtonInput<MouseButton>>,
+    mut mouse_motion: EventReader<MouseMotion>,
+    time: Res<Time>,
+) {
+    let mut transform = camera.single_mut();
+    let mut delta = Vec2::ZERO;
+    for event in mouse_motion.read() {
+        delta += event.delta;
+    }
+    let mut delta_roll = 0.0;
+
+    if mouse_buttons.pressed(MouseButton::Left) {
+        delta_roll -= 1.0;
+    }
+    if mouse_buttons.pressed(MouseButton::Right) {
+        delta_roll += 1.0;
+    }
+
+    // Mouse motion is one of the few inputs that should not be multiplied by delta time,
+    // as we are already receiving the full movement since the last frame was rendered. Multiplying
+    // by delta time here would make the movement slower that it should be.
+    let delta_pitch = delta.y * camera_settings.pitch_speed;
+    let delta_yaw = delta.x * camera_settings.yaw_speed;
+
+    // Conversely, we DO need to factor in delta time for mouse button inputs.
+    delta_roll *= camera_settings.roll_speed * time.delta_seconds();
+
+    // Obtain the existing pitch, yaw, and roll values from the transform.
+    let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+
+    // Establish the new yaw and pitch, preventing the pitch value from exceeding our limits.
+    let pitch = (pitch + delta_pitch).clamp(
+        camera_settings.pitch_range.start,
+        camera_settings.pitch_range.end,
+    );
+    let roll = roll + delta_roll;
+    let yaw = yaw + delta_yaw;
+    transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+
+    // Adjust the translation to maintain the correct orientation toward the orbit target.
+    // In our example it's a static target, but this could easily be customised.
+    let target = Vec3::ZERO;
+    transform.translation = target - transform.forward() * camera_settings.orbit_distance;
 }
